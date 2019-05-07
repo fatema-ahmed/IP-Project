e10a8ba54da38743302026292bd9fefb6b19cfc2
Verina <44680557+Verina-Alber18@users.noreply.github.com>
2019-05-07 09:54:08 +0200

    Update stitching.js

--- code/stitching.js
+++ code/stitching.js
@@ -11,5 +11,39 @@ public class Stitching {
 
 
 
+    static int parseCmdArgs(String[] args) {
+        if (args.length == 0) {
+            printUsage();
+            return -1;
+        }
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--help") || args.equals("/?")) {
+                printUsage();
+                return -1;
+            } else if (args[i].equals("--try_use_gpu")) {
+                if (args[i + 1].equals("no")) {
+                    try_use_gpu = false;
+                } else if (args[i + 1].equals("yes")) {
+                    try_use_gpu = true;
+                } else {
+                    System.out.println("Bad --try_use_gpu flag value");
+                    return -1;
+                }
+                i++;
+            } else if (args[i].equals("--output")) {
+                result_name = args[i + 1];
+                i++;
+            } else {
+                Mat img = imread(args[i]);
+                if (img.empty()) {
+                    System.out.println("Can't read image '" + args[i] + "'");
+                    return -1;
+                }
+                imgs.resize(imgs.size() + 1);
+                imgs.put(imgs.size() - 1, img);
+            }
+        }
+        return 0;
+    }
 
 }
