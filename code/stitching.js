d752311fdcd6b2dd6dac88a7a4145c6c4b402ddf
fatema-ahmed <33998585+fatema-ahmed@users.noreply.github.com>
2019-05-07 09:53:06 +0200

    Create stitching.js

---
+++ code/stitching.js
@@ -0,0 +1,15 @@
+import static org.bytedeco.javacpp.opencv_core.Mat;
+import static org.bytedeco.javacpp.opencv_core.MatVector;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
+import static org.bytedeco.javacpp.opencv_stitching.Stitcher;
+
+public class Stitching {
+    static boolean try_use_gpu = false;
+    static MatVector imgs = new MatVector();
+    static String result_name = "result.jpg";
+
+
+
+
+}
